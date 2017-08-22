711afd1438097b911cb1ee2f8e82c9c91391aae1
jasonlee <jiancheng.li.2011@gmail.com>
2017-08-22 23:59:32 +0200

    feat(Init): init hanlders

---
+++ ActiveRecord/src/zimbra/util/activerecord/ClassOfService.java
@@ -0,0 +1,12 @@
+package zimbra.util.activerecord;
+
+import zimbra.util.activerecord.handler.Handler;
+
+public class ClassOfService extends ActiveRecord {
+
+	public ClassOfService(Handler handler) {
+		super(handler);
+		// TODO Auto-generated constructor stub
+	}
+
+}
